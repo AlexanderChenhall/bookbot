a44602652204097ba87e4721d6d154162dfd5c74
AlexanderChenhall <alexanderchenhall@protonmail.com>
2024-02-23 10:58:31 +1100

    Created function generate_report, moving code from the main function. Also created function to print a string/list of all letter occurances in order of most frequent

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import re
 import string
 def main():
     path_to_file = "books/frankenstein.txt"
-    book_name = get_book_name(path_to_file)
-    print(f"There are {get_number_of_words_in_book(path_to_file)} in the book {book_name}.")
-    print(get_letter_count(path_to_file))
+    generate_report(path_to_file)
+
 
 def get_book_name(path_to_file):
     separated_text = re.split(r'\W+', path_to_file)
@@ -37,6 +36,23 @@ def get_number_of_words_in_book(path_to_file):
     book_text = print_book(path_to_file)
     separated_text = book_text.split()
     return len(separated_text)
+
+def get_sorted_list_from_dict(letter_occurances):
+    sorted_list = sorted(letter_occurances.items(), key=lambda item: item[1], reverse=True)
+    return sorted_list
+
+def generate_report(path_to_file):
+    book_name = get_book_name(path_to_file)
+    sorted_list = get_sorted_list_from_dict(get_letter_count(path_to_file))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"There are {get_number_of_words_in_book(path_to_file)} in the book {book_name}.")
+    print(list_letter_occurances(sorted_list))
+
+def list_letter_occurances(letter_occurances):
+    report_list = ("\nLetters in order of occurance:\n")
+    for elem in letter_occurances:
+        report_list += (f"The '{elem[0]}' character occurs {elem[1]} times\n")
+    return report_list
 
 if __name__ == "__main__":
     main()
